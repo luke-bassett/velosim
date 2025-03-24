ec993b59d9bca3a470d67e6b75b48c5c5d17a7d6
Luke Bassett <bassett.luke@gmail.com>
2025-03-23 21:55:50 -0700

    removed test (for now)

--- src/main.rs
+++ src/main.rs
@@ -2,62 +2,28 @@
 mod vector2d;
 mod physics;
 mod rider;
-mod wind;
 mod simulation;
+mod wind;
 use physics::Velocity;
-use rider::{update_rider_position, update_rider_velocity, Rider};
-use vector2d::Vector2D;
+use rider::Rider;
+use simulation::Simulation;
 use wind::Wind;
 
 /// Density of air at sea level [kg / m^3].
 const DENSITY_OF_AIR_AT_SEA_LEVEL: f64 = 1.225; // kg/m^3
 
 fn main() {
-    let mut riders: Vec<Rider> = vec![Rider::new(300.0, 0.3, 80.0)];
+    let riders: Vec<Rider> = vec![
+        Rider::new(300.0, 0.3, 80.0),
+        Rider::new(280.0, 0.28, 75.0),
+        Rider::new(320.0, 0.32, 85.0),
+    ];
     let dt: f64 = 1.0; // seconds
     let wind = Wind::new(Velocity::new(-5.0, 1.0));
+    let mut simulation = Simulation::new(riders, wind, dt);
 
-    for t in 0..100 {
-        for rider in &mut riders {
-            update_rider_velocity(rider, dt, &wind);
-            update_rider_position(rider, dt);
-        }
-        for rider in &riders {
-            let rider_velocity = rider.velocity();
-
-            println!(
-                "t {:<5} | vx {:>8.2} | vy {:>8.2} | v {:>8.2}",
-                t,
-                rider_velocity.x(),
-                rider_velocity.y(),
-                rider_velocity.magnitude()
-            )
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_velocity_increases_monotonically_from_zero() {
-        let mut rider = Rider::new(300.0, 0.3, 80.0);
-        let wind = Wind::new(Velocity::new(1.0, 0.0));
-
-        let dt: f64 = 1.0;
-
-        for _ in 0..100 {
-            let old_rider_velocity = rider.velocity();
-            update_rider_velocity(&mut rider, dt, &wind);
-            let new_rider_velocity = rider.velocity();
-
-            assert!(
-                new_rider_velocity.x() >= old_rider_velocity.x(),
-                "Velocity decreased! prev: {}, current: {}",
-                old_rider_velocity.x(),
-                new_rider_velocity.x()
-            );
-        }
+    for _ in 0..10 {
+        simulation.tick();
+        simulation.print_rider_positions();
     }
 }
