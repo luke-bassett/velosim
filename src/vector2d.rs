3402ed0f1b3fbcc2941f9f0c6e89e33e3b813933
Luke Bassett <bassett.luke@gmail.com>
2025-03-25 21:40:12 -0700

    add unit tests to vector2d

--- src/vector2d.rs
+++ src/vector2d.rs
@@ -59,3 +59,74 @@ macro_rules! impl_vector2d {
         }
     };
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestVector {
+        x: f64,
+        y: f64,
+    }
+
+    impl_vector2d!(TestVector);
+
+    #[test]
+    fn test_magnitude() {
+        let v = TestVector { x: 3.0, y: 4.0 };
+        assert_eq!(v.magnitude(), 5.0); // 3-4-5 triangle
+    }
+    #[test]
+    fn test_magnitude_negative() {
+        let v = TestVector { x: -3.0, y: -4.0 };
+        assert_eq!(v.magnitude(), 5.0); // 3-4-5 triangle
+    }
+
+
+    #[test]
+    fn test_unit() {
+        let v = TestVector { x: 3.0, y: 3.0 };
+        let unit = v.unit();
+        assert_eq!(unit.magnitude(), 1.0);
+        assert!((unit.x() - unit.y()).abs() < f64::EPSILON);
+        assert!((unit.x() - (2.0_f64.sqrt() / 2.0)).abs() < f64::EPSILON);
+    }
+    #[test]
+    fn test_unit_of_zero() {
+        let v = TestVector { x: 0.0, y: 0.0 };
+        let unit = v.unit();
+        assert_eq!(unit.magnitude(), 0.0);
+    }
+
+    #[test]
+    fn test_scale() {
+        let v = TestVector { x: 3.0, y: 4.0 };
+        let scaled = v.scale(2.0);
+        assert_eq!(scaled.magnitude(), 10.0);
+        assert_eq!(scaled.x(), 6.0);
+        assert_eq!(scaled.y(), 8.0);
+    }
+
+    #[test]
+    fn test_add() {
+        let v1 = TestVector { x: 3.0, y: 4.0 };
+        let v2 = TestVector { x: 1.0, y: 2.0 };
+        let added = v1.add(&v2);
+        assert_eq!(added.x(), 4.0);
+        assert_eq!(added.y(), 6.0);
+    }
+    #[test]
+    fn test_sub() {
+        let v1 = TestVector { x: 3.0, y: 4.0 };
+        let v2 = TestVector { x: 1.0, y: 2.0 };
+        let subbed = v1.sub(&v2);
+        assert_eq!(subbed.x(), 2.0);
+        assert_eq!(subbed.y(), 2.0);
+    }
+    #[test]
+    fn test_from_components() {
+        let v = TestVector::from_components(1.0, 2.0);
+        assert_eq!(v.x(), 1.0);
+        assert_eq!(v.y(), 2.0);
+    }
+}
